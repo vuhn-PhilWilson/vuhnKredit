abffbf1f9e3e8272724ac06cbfda95bbde4e9de9
Phil Wilson <number@vu.hn>
2020-02-20 19:16:21 +1100

    update vuhnNetwork to 0.0.4

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "vuhnKredit",
     dependencies: [
-        .package(path: "../vuhnNetwork")
-//        .package(url: "https://github.com/vuhn-PhilWilson/vuhnNetwork", from: "0.0.3")
+//        .package(path: "../vuhnNetwork")
+        .package(url: "https://github.com/vuhn-PhilWilson/vuhnNetwork", from: "0.0.4")
     ],
     targets: [
         .target(
